dc6f0ef902ea4ce2e5cd8e681289b24f2ce46337
Asymmentric <74967082+Asymmentric@users.noreply.github.com>
2022-02-16 16:11:28 +0530

    Modified as per .env vars

--- server/pools.js
+++ server/pools.js
@@ -4,10 +4,10 @@ const dotenv=require('dotenv');
 
 let pool=mysql.createPool({
     connectionLimit:100,
-    host:'localhost',
-    user:'root',
-    password:'1234',
-    database:'express01'
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 })
 
 module.exports=pool;
